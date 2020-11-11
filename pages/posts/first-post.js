f0e5975452cc0c6ecb3e4391433f5b308267bc99
love-adelar <love.adelar@gmail.com>
2020-11-11 14:05:25 +0900

    title, og tag 설정

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function FirstPost() {
   return (
   <>
+    <Head>
+      <title>First Post</title>
+      <meta property="og:title" content="My page title" key="title" />
+    </Head>
     <h1>First Post</h1>
     <h2>
       <Link href="/">
