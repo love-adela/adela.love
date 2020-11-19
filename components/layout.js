9fe424aeb8228626054b0add20b4ce18f3df2317
love-adelar <love.adelar@gmail.com>
2020-11-19 18:39:20 +0900

    Add layout and layout module

---
+++ components/layout.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+const name = 'Adela'
+export const siteTitle = 'Next.js Sample Website'
+
+export default function Layout({ children, home }) {
+  return (
+  <div className={styles.container}>
+  <Head>
+    <link rel="icon" href="/favicon.ico" />
+    <meta 
+      name="description"
+      content="Learn how to build a personal website using Next.js"
+    />
+    <meta
+      property="og:image"
+      cotent={`https://og-image.now.sh/${encodeURI(
+        siteTitle
+      )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+    />
+    <meta name="og:title" content={siteTitle} />
+    <meta name="twitter:card" content="summary_large_image" />  
+  </Head>
+  
+  
+  <main>{children}</main>
+  
+  </div>
+  )
+}
