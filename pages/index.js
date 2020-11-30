135d6544defb1da1a249b8cd0e7d3174eef35708
love-adelar <love.adelar@gmail.com>
2020-11-30 23:38:18 +0900

    자기소개 요약 삭제하고 부제 보이게 함

--- pages/index.js
+++ pages/index.js
@@ -12,24 +12,24 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>안녕하세요, 아델라입니다.</p>
-        <p>
-          (This is a sample website - you'll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, date, title, subtitle }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
-                <a>{title}</a>
+                <a><h2>{title}</h2></a>
               </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date}/>
-              </small>
+              <Link href={`/posts/${id}`}>
+                <a><h3>{subtitle}</h3></a>
+              </Link>
+              <Link href={`/posts/${id}`}>
+                <a>
+                  <small className={utilStyles.lightText}>
+                    <Date dateString={date}/>
+                  </small>
+                </a>
+              </Link>
             </li>
           ))}
         </ul>
