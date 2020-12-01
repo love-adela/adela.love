1dfa8cb5e96e3c8e68af88cc085abfa3cb169a74
love-adelar <love.adelar@gmail.com>
2020-12-01 22:50:20 +0900

    Add about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+import Link from 'next/link'
+
+export default function About() {
+  return (
+    <Layout>
+      <div>About</div>
+    </Layout>
+  )
+}
