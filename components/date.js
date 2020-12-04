4f729db840c6053b284f9ebb1e5dfc109a96399b
love-adelar <love.adelar@gmail.com>
2020-12-05 06:10:52 +0900

    포스트에 날짜 안 적으면 사이트 터지는 문제 해결

--- components/date.js
+++ components/date.js
@@ -1,6 +1,7 @@
 import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
+  if (dateString == null) { return null }
   const date = parseISO(dateString)
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
 }
