fc857a7c95511f2229edc89638475dd1df6d6e31
love-adelar <love.adelar@gmail.com>
2020-12-08 02:18:43 +0900

    Modify open graph load path

--- components/layout.js
+++ components/layout.js
@@ -20,7 +20,7 @@ export default function Layout({ children, home }) {
           property="og:image"
           content={`https://og-image.now.sh/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=light&md=0&fontSize=75px&images=https://raw.githubusercontent.com/love-adela/adela.love/main/public/images/og_image.jpg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
