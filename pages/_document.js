4a8d114ff4cfc40d154d7bd5a720c7f3be7bbd45
love-adelar <love.adelar@gmail.com>
2020-12-08 18:22:35 +0900

    Add title tag in html tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { siteTitle } from '../components/layout'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,7 +10,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="kr">
-        <Head />
+        <Head>
+        <title>adela.love | {siteTitle}</title>
+        </Head>
         <body>
           <Main />
           <NextScript />
