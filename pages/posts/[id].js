989178341df12b459908104503c1c46c2bc26ce6
Jiyun Chung <jiyun@dable.io>
2021-12-24 13:33:20 +0900

    Clarify copyright: CC by - nc - nd
    * 출처 표기 / 비상업적 이용 / derived work불가

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,6 +5,12 @@ import utilStyles from '../../styles/utils.module.css'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
+  const license = 
+  `<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+    <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" />
+  </a>
+  <br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.`
+
   return (
     <Layout>
       <Head>
@@ -22,6 +28,7 @@ export default function Post({ postData }) {
         </small>
         </div>
         <div className={utilStyles.markdownText} dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div className={utilStyles.markdownText} dangerouslySetInnerHTML={{ __html: license }} />
       </article>
     </Layout>
   )
