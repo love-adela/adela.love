1ed8d6933f50b7186ff60734872067ac76909d06
Jiyun Chung <jiyun@dable.io>
2022-01-02 20:40:20 +0900

    Remove useless whitespaces

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-import '../styles/global.css'
-import '../styles/highlight.css'
+import '../styles/reset.css';
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
-}
+  }
