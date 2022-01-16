0068c8d31006b7a9c80f2ddbbc662e3dc956b58f
Jiyun Chung <jiyun@dable.io>
2022-01-16 16:44:34 +0900

    Revert "Change order"
    This reverts commit 2da7b100fefd4c12ac7fbbe93bfa72e3434d388d.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/reset.css'
 import '../styles/global.css'
+import '../styles/reset.css'
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
