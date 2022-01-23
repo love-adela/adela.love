17415e1136af4136dba187dba9e9e4c9c90829d7
Jiyun Chung <jiyun@dable.io>
2022-01-23 23:37:37 +0900

    Align the code

--- pages/index.js
+++ pages/index.js
@@ -5,31 +5,31 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <Layout home>
-      <article class={utilStyles.homeIntroduction}>
-        <div class={utilStyles.homeResume}>
-          <div class={utilStyles.resumetitle}>
-            <h3 class={utilStyles.resumeItem}>Experience</h3>
-            <div class={utilStyles.resumeSpec}>
+      <article className={utilStyles.homeIntroduction}>
+        <div className={utilStyles.homeResume}>
+          <div className={utilStyles.resumetitle}>
+            <h3 className={utilStyles.resumeItem}>Experience</h3>
+            <div className={utilStyles.resumeSpec}>
               <p>Data Engineer</p>
               <Link href="https://dable.io">
-              <a class={utilStyles.resumeCompany}>Dable</a>
+              <a className={utilStyles.resumeCompany}>Dable</a>
               </Link>
               <p>Feb, 2021 - Present</p>
             </div>
           </div>
           <br/>
-          <div class={utilStyles.resumetitle}>
-            <h3 class={utilStyles.resumeItem}>Education</h3>
-            <div class={utilStyles.resumeSpec}>
+          <div className={utilStyles.resumetitle}>
+            <h3 className={utilStyles.resumeItem}>Education</h3>
+            <div className={utilStyles.resumeSpec}>
               <p>Computer Engineering & Science</p>
               <p>Sungshin Women's University</p>
               <p>Mar, 2014 - Feb, 2019</p>
             </div>
           </div>
           <br/>
-          <div class={utilStyles.resumetitle}>
-            <h3 class={utilStyles.resumeItem}>Tech Stack</h3>
-            <div class={utilStyles.resumeSpec}>
+          <div className={utilStyles.resumetitle}>
+            <h3 className={utilStyles.resumeItem}>Tech Stack</h3>
+            <div className={utilStyles.resumeSpec}>
               <p>Python3</p>
             </div>
           </div>
