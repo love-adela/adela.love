a77be5067c4419bca409c849dd05dba8cb04b909
Jiyun Chung <jiyun@dable.io>
2022-01-30 19:21:06 +0900

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,10 @@ function App({ Component, pageProps }) {
     <>
       {/* favicon */}
       <Head>
-        <link rel="favicon" href="/favicon.png" />
+        <title>adela.love</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
       </Head>
       <Component {...pageProps} />
     </>
